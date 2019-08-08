8b83116a1c0df931561d812256ecccb902a6776a
TamerNoami <tamernoami@gmail.com>
2019-08-08 20:43:11 +0200

    Adding OrderType enum

---
+++ price-component/price-component-domain/src/main/java/se/lexicon/price/component/domain/OrderTypes.java
@@ -0,0 +1,5 @@
+package se.lexicon.price.component.domain;
+
+public enum OrderTypes {
+ Market_ORDER, Limit_ORDER, Day_ORDER, Stop_Loss_ORDER, Sell_Stop_ORDER, Buy_Stop_ORDER, Stop_limit_Order, Trailing_Stop_ORDER
+}
